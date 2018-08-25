571b2f02c447f68b5752a488f9a0cf974b739041
Yoga Prihatno <41616827+minitype@users.noreply.github.com>
2018-08-25 18:57:02 +0700

    Update src.js

--- checkPalindrome/src.js
+++ checkPalindrome/src.js
@@ -1,3 +1,6 @@
+//Given the string, check if it is a palindrome.
+//A palindrome is a string that reads the same left-to-right and right-to-left.
+
 function checkPalindrome(inputString) {
     let j = inputString.length - 1;
     for (let i = 0; i < inputString.length/2; i++) {
